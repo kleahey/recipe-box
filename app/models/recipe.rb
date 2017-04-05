9f7637b6efca39283a4ffb228d5a539c53bc2aca
Kevin Leahey <kleahey@me.com>
2017-04-05 15:38:14 -0400

    Bootstrap navbar and new Recipe index layout

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
-  has_attached_file :image, styles: { medium: "400x400#", thumb: "150x150#"}
+  has_attached_file :image, styles: { medium: "400x400#", thumb: "300x200#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
