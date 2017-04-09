88adf81bde9943033989bad1dc47b14aea420cc3
Kevin Leahey <kleahey@me.com>
2017-04-09 10:46:47 -0400

    More Heroku tweaks

--- db/migrate/20170405053835_remove_field_from_recipes.rb
+++
@@ -1,6 +0,0 @@
-class RemoveFieldFromRecipes < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :recipes, :main_image, :string
-    remove_column :recipes, :thumb_image, :string
-  end
-end
