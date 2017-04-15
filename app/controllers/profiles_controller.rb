2c487b5c196223a522198bd2beff1b519edacf94
Kevin Leahey <kleahey@me.com>
2017-04-15 15:22:16 -0400

    Profile controller updated for authentication

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,16 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
+   before_action :set_user
+   before_action :owned_profile, only: [:edit, :update]
 
   def show
-    @user = User.find_by(user_name: params[:user_name])
   end
 
   def edit
-    @user = User.find_by(user_name: params[:user_name])
   end
 
   def update
     respond_to do |format|
-      @user = User.find_by(user_name: params[:user_name])
         if @user.update(profile_params)
           format.html { redirect_to profile_path(@user.user_name), notice: "Your profile was updated successfully!" }
         else
@@ -26,6 +26,17 @@ class ProfilesController < ApplicationController
 
   def profile_params
     params.require(:user).permit(:avatar, :bio)
+  end
+
+  def owned_profile
+    unless current_user == @user
+      flash[:alert] = "That profile doesn't belong to you!"
+      redirect_to root_path
+    end
+  end
+
+  def set_user
+    @user = User.find_by(user_name: params[:user_name])
   end
 
 end
