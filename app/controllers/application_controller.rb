26375f30e724ce81155df43c2d915846d2872d0e
Kevin Leahey <kleahey@me.com>
2017-04-15 15:46:19 -0400

    DRY cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  skip_before_filter :verify_authenticity_token
+
   def after_sign_in_path_for(user)
    user_recipes_path(current_user.user_name)
   end
